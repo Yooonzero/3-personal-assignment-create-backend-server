38655a31c4966de6dddc73baf3c0dd06493b57ac
yooonzero <playlist808@kakao.com>
2023-06-16 10:28:20 +0900

    first commit all files

---
+++ schemas/comment.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+
+const commentSchema = new mongoose.Schema({
+    user: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    password: {
+        type: Number,
+        required: true,
+        unique: true,
+    },
+    content: {
+        type: String,
+        required: true,
+    },
+    createdAt: {
+        type: Date,
+        default: Date.now,
+    },
+});
+
+module.exports = mongoose.model('Comments', commentSchema);
