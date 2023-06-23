eff43b8c80e6387c2af7386f8f79d1bf752776d8
yooonzero <playlist808@kakao.com>
2023-06-23 12:24:00 +0900

    디렉토리명 변경

--- routes/posts.js
+++ routes/posts.js
@@ -12,11 +12,10 @@ router.post('/posts', async (req, res) => {
             title,
             content,
         });
-
         res.send({ message: '게시글을 작성하였습니다.' }).json({ post: createdPost });
     } catch (error) {
-        // console.log(error);
-        // res.status(400).send({ msg: '데이터 형식이 올바르지 않습니다.' });
+        console.log(error);
+        res.status(400).send({ msg: '데이터 형식이 올바르지 않습니다.' });
         // 위 코드를 활성화 시키면, Error [ERR_HTTP_HEADERS_SENT]: Cannot set headers after they are sent to the client 라는 오류가 출력.
     }
 });
